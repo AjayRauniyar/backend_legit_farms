38e7a9633c92eec1feeeedf9d48a72a5f068c3f4
AjayRauniyar <winningaj77@gmail.com>
2024-08-11 17:27:39 +0530

    edit

--- services/otpServices.js
+++ services/otpServices.js
@@ -1,7 +1,5 @@
 const axios = require('axios');
-const axiosRetry = require('axios-retry');
 
-axiosRetry(axios, { retries: 3 });
 const config = require('../config/config');
 
 const sendOTP = async (mobileNumber) => {
